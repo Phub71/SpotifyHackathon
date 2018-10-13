f87b5ae9714cea675cbb77266117e560638501cd
Rikard Legge <rikard.legge@gmail.com>
2018-10-13 10:27:51 +0200

    Add database commands

--- server/database.js
+++ server/database.js
@@ -1,24 +1,65 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
-function createDatabase() {
-  db.serialize(function() {
-    db.run("CREATE TABLE lorem (info TEXT)");
+function create() {
+  db.serialize(function () {
+    db.run("CREATE TABLE users (user_id TEXT, name TEXT)");
+    db.run("CREATE TABLE songs (user_id TEXT, track_id TEXT, happy_emotion int DEFAULT 0, sad_emotion int DEFAULT 0)");
+    db.run("CREATE TABLE playlists (user_id TEXT, playlist_id TEXT)");
+  });
+}
 
-    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
-
-    db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
+function listSongs() {
+  return new Promise((resolve, reject) => {
+    db.all("SELECT user_id, track_id, happy_emotion, sad_emotion FROM songs", function (error, rows) {
+      if (error) reject(error);
+      resolve(rows);
     });
   });
 }
 
-function closeDatabase() {
+function addSong(userId, trackId) {
+  return new Promise((resolve, reject) => {
+    db.run(`INSERT INTO songs (user_id, track_id) VALUES (?, ?);`, userId, trackId,
+      (error) => {
+        if (error) reject(error);
+        resolve();
+      });
+  });
+}
+
+function removeSong(userId, trackId) {
+  return new Promise((resolve, reject) => {
+    db.run(`DELETE FROM songs WHERE user_id = ? AND track_id = ?;`, userId, trackId,
+      (error) => {
+        if (error) reject(error);
+        resolve();
+      });
+  });
+}
+
+function reactHappy(userId, trackId) {
+  return new Promise((resolve, reject) => {
+    db.run(`UPDATE songs SET happy_emotion = happy_emotion + 1 WHERE user_id = ? AND track_id = ?;`, userId, trackId,
+      (error) => {
+        if (error) reject(error);
+        resolve();
+      });
+  });
+}
+
+function reactSad(userId, trackId) {
+  return new Promise((resolve, reject) => {
+    db.run(`UPDATE songs SET sad_emotion = sad_emotion + 1 WHERE user_id = ? AND track_id = ?;`, userId, trackId,
+      (error) => {
+        if (error) reject(error);
+        resolve();
+      });
+  });
+}
+
+function close() {
   db.close();
 }
 
-module.exports = {createDatabase, closeDatabase};
+module.exports = {create, close, listSongs, addSong, removeSong, reactHappy, reactSad};
