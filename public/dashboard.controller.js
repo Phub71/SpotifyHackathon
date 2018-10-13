128aa2b1ae2141050e145d9b914466d7ea089d55
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2018-10-13 14:56:35 +0200

    additions

--- public/dashboard.controller.js
+++ public/dashboard.controller.js
@@ -1,31 +1,13 @@
-// // import {get, post} from "./network.js";
-//
-// angular.module('spotifyApp', ['ngMaterial', 'ngMessages', '$resourceProvider'])
-//     .controller("MainController", [function($resource){
-//
-//        $resource.get();
-//
-//         var tracks = $resource('/listSongs',);
-//         tracks.get().then(function(res) {
-//            console.log(res)
-//         });
-//
-//     }]);
-//
-//
-//
+import {get, post} from "./network.js";
 
 angular.module('spotifyApp')
     .controller('MainController', [
-        function () {
-            console.log(1234)
-            // $resource.get();
-            //
-            // var tracks = $resource('/listSongs',);
-            // tracks.get().then(function (res) {
-            //     console.log(res)
-            // });
+        async function () {
 
+            var self = this;
+            self.tracks = [];
+            self.tracks = await get('/listSongs');
+            console.log(self.tracks)
 
         }
 
