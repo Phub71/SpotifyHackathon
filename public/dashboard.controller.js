2c0de30483d494eb89ca76216ca863f9ba4b0e2a
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2018-10-13 16:05:21 +0200

    implementing dashboard

--- public/dashboard.controller.js
+++ public/dashboard.controller.js
@@ -1,14 +1,42 @@
 import {get, post} from "./network.js";
 
 angular.module('spotifyApp')
-    .controller('MainController', [
-        async function () {
-
+    .controller('MainController', ['$timeout', '$scope',
+        async function ($timeout, $scope) {
             var self = this;
-            self.tracks = [];
-            self.tracks = await get('/listSongs');
-            console.log(self.tracks)
+            $scope.loading = true;
 
+            self.refresh = function(){
+                get('/listSongs').then(function(res){
+                    $scope.tracks = res;
+                    $scope.loading = false;
+                });
+            };
+
+            $scope.addSong = function () {
+                console.log(798)
+            };
+
+            $scope.findTracks = async function () {
+                post('search-track', {name: $scope.newSong}).then(function(res){
+                    $scope.newTracks = res;
+                });
+            };
+
+            $scope.postSong = function(id){
+                $scope.loading = true;
+                post('/addSong', {trackId: id}).then(
+                    function(){
+                        $scope.loading = true;
+                        $scope.newTracks = [];
+                        self.refresh();
+                    }
+                );
+            };
+
+
+
+            self.refresh();
         }
 
     ]);
