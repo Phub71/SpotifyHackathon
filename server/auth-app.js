025e2fe013fe56d230a5f286088ab50c0f3fb19e
Rikard Legge <rikard.legge@gmail.com>
2018-10-13 13:40:57 +0200

    Fix callback in auth

--- server/auth-app.js
+++ server/auth-app.js
@@ -19,14 +19,14 @@ require('dotenv').config();
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = 'http://localhost:8888/dashboard.html'; // Your redirect uri
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
+var generateRandomString = function (length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -36,9 +36,9 @@ var generateRandomString = function(length) {
   return text;
 };
 
-function init(app){
+function init(app) {
 
-  app.get('/login', function(req, res) {
+  app.get('/login', function (req, res) {
 
     var state = generateRandomString(16);
     res.cookie(stateKey, state);
@@ -55,7 +55,7 @@ function init(app){
       }));
   });
 
-  app.get('/callback', function(req, res) {
+  app.get('/callback', function (req, res) {
 
     // your application requests refresh and access tokens
     // after checking the state parameter
@@ -84,46 +84,39 @@ function init(app){
         json: true
       };
 
-      request.post(authOptions, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
+      request.post(authOptions, function (error, response, body) {
+          if (error || response.statusCode !== 200) {
+            res.redirect('/failed');
+            return;
+          }
 
-          var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-          var options = {
+          const {access_token, refresh_token} = body;
+          const options = {
             url: 'https://api.spotify.com/v1/me',
-            headers: { 'Authorization': 'Bearer ' + access_token },
+            headers: {'Authorization': 'Bearer ' + access_token},
             json: true
           };
 
           // use the access token to access the Spotify Web API
-          request.get(options, function(error, response, body) {
+          request.get(options, function (error, response, body) {
             console.log(body);
+            res.cookie('access_token', access_token);
+            res.cookie('user_id', body.id);
+            res.redirect('/dashboard');
           });
-
-          // we can also pass the token to the browser to make requests from there
-          res.redirect('/#' +
-            querystring.stringify({
-              access_token: access_token,
-              refresh_token: refresh_token
-            }));
-        } else {
-          res.redirect('/#' +
-            querystring.stringify({
-              error: 'invalid_token'
-            }));
         }
-      });
+      );
     }
-  });
+  })
+  ;
 
-  app.get('/refresh_token', function(req, res) {
+  app.get('/refresh_token', function (req, res) {
 
     // requesting access token from refresh token
     var refresh_token = req.query.refresh_token;
     var authOptions = {
       url: 'https://accounts.spotify.com/api/token',
-      headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+      headers: {'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))},
       form: {
         grant_type: 'refresh_token',
         refresh_token: refresh_token
@@ -131,7 +124,7 @@ function init(app){
       json: true
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         var access_token = body.access_token;
         res.send({
