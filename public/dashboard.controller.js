bc122b4ac568f7c57d08b9f48e8eb9f82bb38af9
Rikard Legge <rikard.legge@gmail.com>
2018-10-13 18:41:15 +0200

    Auto redraw ui on get and post

--- public/dashboard.controller.js
+++ public/dashboard.controller.js
@@ -3,24 +3,35 @@ import {play,pause} from "./player.js";
 
 angular.module('spotifyApp')
     .controller('MainController', ['$timeout', '$scope',
-        async function ($timeout, $scope) {
+        function ($timeout, $scope) {
             var self = this;
 
             self.refresh = function(){
                 $scope.loading = true;
-                get('/listSongs').then(function(res){
+              $scope.get('/listSongs').then(function(res){
                     $scope.tracks = res;
                     $scope.loading = false;
                 });
             };
-
+          $scope.get = function(url) {
+            return get(url).then((data)=>{
+              $timeout(1, ()=>$scope.apply());
+              return data
+            })
+          };
+          $scope.post = function(url, data) {
+            return post(url, data).then((data)=>{
+              $timeout(1, ()=>$scope.apply());
+              return data
+            })
+          };
             $scope.searchForSong = function(){
                 $scope.findTracks();
             };
 
             $scope.findTracks = function () {
                 $scope.loading = true;
-                post('search-track', {name: $scope.newSong}).then(function(res){
+              $scope.post('search-track', {name: $scope.newSong}).then(function(res){
                     $scope.newTracks = res;
                     $scope.loading = false;
                 });
